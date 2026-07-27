f74dc1b6116fd3275e5f27b060c47da1ae75e4d3
jinhyeong jang <jinhinjala26@naver.com>
2026-07-27 13:17:05 +0900

    fix: 회원정보 수정 PATCH CORS 허용 (#40)

--- src/main/java/inu/timetable/config/CorsConfig.java
+++ src/main/java/inu/timetable/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
